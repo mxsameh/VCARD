1f31435eff742770196258d193a951b58e341249
Mohamed Sameh <mxsameh@gmail.com>
2025-08-01 04:42:42 +0300

    sync

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,18 @@
 import s from "./_s.module.scss";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div className={s.p}>
+      <h1 className={s.lo}>GASHX</h1>
+      <div className={s.l}>
+        <h2>DUAL SOULS</h2>
+      </div>
+      <div className={s.r}>
+        <h2>ONE FLAME</h2>
+      </div>
+      <h1 className={s.co}>COMMING SOON</h1>
+    </div>
+  );
 };
 
 export default Home;
