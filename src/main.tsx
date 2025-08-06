79422f499496bbd1b9f76979e455456a16d3335a
Mohamed Sameh <mxsameh@gmail.com>
2025-08-06 11:05:17 +0300

    sync

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import { ReactLenis } from "@studio-freight/react-lenis";
 import "@s/global/index.scss";
 import App from "./App.tsx";
-import Grid from "@c/grid/index.tsx";
+// import Grid from "@c/grid/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <Grid />
-        <ReactLenis root>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </ReactLenis>
-    </React.StrictMode>
+  <React.StrictMode>
+    {/* <Grid /> */}
+    <ReactLenis root>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactLenis>
+  </React.StrictMode>
 );
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,18 +1,7 @@
 import s from "./_s.module.scss";
 
 const Home = () => {
-  return (
-    <div className={s.p}>
-      <h1 className={s.lo}>GASHX</h1>
-      <div className={s.l}>
-        <h2>DUAL SOULS</h2>
-      </div>
-      <div className={s.r}>
-        <h2>ONE FLAME</h2>
-      </div>
-      <h1 className={s.co}>COMMING SOON</h1>
-    </div>
-  );
+  return <div className={s.p}></div>;
 };
 
 export default Home;
