4824f4392975655e1f1baa06c4a0f7499f5a5188
Mohamed Sameh <mxsameh@gmail.com>
2025-08-27 16:25:51 +0300

    sync

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import { ReactLenis } from "@studio-freight/react-lenis";
 import "@s/global/index.scss";
 import App from "./App.tsx";
-// import Grid from "@c/grid/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <Grid /> */}
     <ReactLenis root>
       <BrowserRouter>
         <App />
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,17 @@
 import s from "./_s.module.scss";
 
 const Home = () => {
-  return <div className={s.p}></div>;
+  const handleClick = () => {
+    console.log("hello");
+  };
+
+  return (
+    <div className={s.p}>
+      <button onClick={handleClick}>
+        <a href="/rim.vcf">download</a>
+      </button>
+    </div>
+  );
 };
 
 export default Home;
